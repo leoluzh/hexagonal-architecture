81807d4ab9f3b6cba66a44b5f8b8371e43e806a0
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-07-19 00:43:31 -0300

    mapeamento atraves de qualifier

--- src/main/java/com/lambdasys/hexagonal/architecture/mappers/StoreEntityMapper.java
+++ src/main/java/com/lambdasys/hexagonal/architecture/mappers/StoreEntityMapper.java
@@ -6,6 +6,8 @@ import com.lambdasys.hexagonal.architecture.application.domain.Location;
 import com.lambdasys.hexagonal.architecture.application.domain.Store;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper
 public interface StoreEntityMapper {
@@ -13,21 +15,33 @@ public interface StoreEntityMapper {
     @Mapping(target = "id", source = "source.id")
     @Mapping(target = "name", source = "source.name")
     @Mapping(target = "zipCode", source = "source.location.zipCode")
-    @Mapping(target = "geoJSONPoint", source = "source")
+    @Mapping(target = "geoJSONPoint", source = "source", qualifiedByName = "mapGeoJSONPoint")
     StoreEntity toEntity(final Store source);
 
+
+//    default void mapGeoJSONPoint(final Store source, @MappingTarget StoreEntity target) {
+//        target.setGeoJSONPoint(toGeoJSONPoint(source));
+//    }
+
+
+    @Named("mapGeoJSONPoint")
     default GeoJSONPoint toGeoJSONPoint(final Store store) {
         return GeoJSONPoint.builder()
                 .coordinate(store.getLocation().getLongitude())
                 .coordinate(store.getLocation().getLatitude())
                 .build();
     }
-    
+
     @Mapping(target = "id", source = "source.id")
     @Mapping(target = "name", source = "source.name")
-    @Mapping(target = "location", source = "source")
+    @Mapping(target = "location", source = "source", qualifiedByName = "mapLocation")
     Store toDomain(final StoreEntity source);
 
+//    default void mapLocation(final StoreEntity source, @MappingTarget Store target){
+//        target.setLocation(toLocation(source));
+//    }
+
+    @Named("mapLocation")
     default Location toLocation(final StoreEntity source) {
         return Location
                 .builder()
